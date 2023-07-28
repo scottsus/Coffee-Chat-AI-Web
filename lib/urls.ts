23cd1c00881655c678e6eda34663453ce39432ca
Wilson Lim Setiawan <wilsonlimsetiawan@gmail.com>
2023-07-28 15:10:55 -0700

    new url

--- lib/urls.ts
+++ lib/urls.ts
@@ -1,2 +1,2 @@
-export const TEST_URL = 'https://api.render.com/deploy/srv-cino5mdgkuvudi9n2qbg?key=CqNPF-GIsEY';
+export const TEST_URL = 'https://api.render.com/deploy/srv-cino5mdgkuvudi9n2qbg';
 export const PROD_URL = process.env.PROD_URL;
